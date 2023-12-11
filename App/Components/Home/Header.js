8df3c0a77852ec8a03f66c42e721ec629282859a
Snowi <snowicambronero@gmail.com>
2023-12-11 13:32:42 +0800

    added shared file

--- App/Components/Home/Header.js
+++ App/Components/Home/Header.js
@@ -1,10 +1,57 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  Dimensions,
+} from "react-native";
 import React from "react";
+import Color from "../../Shared/Color";
 
 export default function Header() {
   return (
-    <View>
-      <Text>Header Component</Text>
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gap: 10,
+        alignItems: "center",
+      }}
+    >
+      <Image
+        source={require("./../../../assets/logo.png")}
+        style={styles.logo}
+      />
+
+      <View>
+        <TextInput placeholder="Search" style={styles.searchBar} />
+      </View>
+      <Image
+        source={require("./../../../assets/placeholder.jpg")}
+        style={styles.userImg}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 50,
+    height: 50,
+  },
+  searchBar: {
+    borderWidth: 1,
+    borderColor: Color.black,
+    padding: 4,
+    borderRadius: 50,
+    paddingLeft: 10,
+    width: Dimensions.get("screen").width * 0.5,
+  },
+  userImg: {
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+  },
+});
